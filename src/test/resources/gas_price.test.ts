bdadd9595c20c5e42403d71755ed2886fab0cf5b
Petar Vujović <petarvujovic98@gmail.com>
2022-01-31 08:50:30 +0100

    fix: Update gas price test
    Move test to inner describe for get
    Separate two test cases, with and without block ID

--- src/test/resources/gas_price.test.ts
+++ src/test/resources/gas_price.test.ts
@@ -7,42 +7,51 @@ import { GetGasPrice, GasPriceResult } from "../../lib/resources/gas_price";
 describe("gas_price", () => {
   let appTester: ReturnType<typeof createAppTester>;
 
-  beforeEach(() => {
-    appTester = createAppTester(App);
-    tools.env.inject();
-  });
+  describe("get", () => {
+    let perform: PureFunction<GetGasPrice, GasPriceResult>;
 
-  test("should return value with up to date timestamp", async () => {
-    const before = new Date().getTime();
+    beforeEach(() => {
+      appTester = createAppTester(App);
+      tools.env.inject();
+      perform = App.resources.gas_price.get.operation.perform as PureFunction<
+        GetGasPrice,
+        GasPriceResult
+      >;
+    });
 
-    const perform = App.resources.gas_price.get.operation
-      .perform as PureFunction<GetGasPrice, { id: string; gas_price: string }>;
+    test("should return value with up to date timestamp", async () => {
+      const before = new Date().getTime();
 
-    const result = (await appTester(perform, {
-      inputData: { block_id: "" },
-    })) as GasPriceResult;
+      const result = (await appTester(perform, {
+        inputData: {},
+      })) as GasPriceResult;
 
-    const after = new Date().getTime();
+      const after = new Date().getTime();
 
-    expect(result).toBeDefined();
+      expect(result).toBeDefined();
 
-    const { gas_price, id } = result;
+      const { gas_price, id } = result;
 
-    expect(typeof gas_price).toEqual("string");
+      expect(typeof gas_price).toEqual("string");
 
-    expect(new Date(id).getTime()).toBeGreaterThanOrEqual(before);
-    expect(new Date(id).getTime()).toBeLessThanOrEqual(after);
+      const timestamp = new Date(id).getTime();
 
-    const result2 = (await appTester(perform, {
-      inputData: {
-        block_id: 80479688,
-      },
-    })) as GasPriceResult;
+      expect(timestamp).toBeGreaterThanOrEqual(before);
+      expect(timestamp).toBeLessThanOrEqual(after);
+    });
 
-    expect(result2).toBeDefined();
+    test("should return value for given block ID", async () => {
+      const result = (await appTester(perform, {
+        inputData: {
+          block_id: 80479688,
+        },
+      })) as GasPriceResult;
 
-    const { gas_price: gas_price2 } = result2;
+      expect(result).toBeDefined();
 
-    expect(typeof gas_price2).toEqual("string");
+      const { gas_price } = result;
+
+      expect(typeof gas_price).toEqual("string");
+    });
   });
 });
