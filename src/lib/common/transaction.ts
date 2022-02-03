b767dad4460c02b7db73baafcbb10e9285dea745
Petar Vujović <petarvujovic98@gmail.com>
2022-02-03 19:08:00 +0100

    feat: Add encoding function
    Add helper function for encoding strings to Uint8Arrays

--- src/lib/common/transaction.ts
+++ src/lib/common/transaction.ts
@@ -13,3 +13,34 @@ export const TXHashField: Field = {
 export interface WithTXHash {
   txHash: string;
 }
+
+/**
+ * Encode a transaction hash field into an Uint8Array.
+ *
+ * @param  {string} txHash
+ * @returns Uint8Array
+ */
+export function encodeToU8(txHash: string): Uint8Array;
+/**
+ * Encode a transaction hash from an object that includes it into an Uin8Array.
+ *
+ * @param  {WithTXHash} {txHash}
+ * @returns Uint8Array
+ */
+export function encodeToU8({ txHash }: WithTXHash): Uint8Array;
+/**
+ * Encode a transaction hash into an Uint8Array.
+ *
+ * @param  {string|WithTXHash} param
+ * @returns Uint8Array
+ */
+export function encodeToU8(param: string | WithTXHash): Uint8Array {
+  const hash = typeof param === "string" ? param : param.txHash;
+  const array = new Uint8Array(hash.length);
+
+  hash.split("").forEach((character, index) => {
+    array[index] = character.charCodeAt(0);
+  });
+
+  return array;
+}
