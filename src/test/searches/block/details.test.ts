c305e30f6416a8279397912edcba0778cfd5b0a1
Petar Vujović <petarvujovic98@gmail.com>
2022-02-03 12:29:34 +0100

    chore: Refactor block details test
    Refactor block details tests to use same structure as others

--- src/test/searches/block/details.test.ts
+++ src/test/searches/block/details.test.ts
@@ -2,21 +2,29 @@ import { createAppTester, tools } from "zapier-platform-core";
 
 import App from "../../..";
 import { Finality } from "../../../lib/common";
-import { perform as blockPerform } from "../../../lib/searches/block/details";
+import {
+  BlockDetailsInput,
+  BlockDetailsResponse,
+  perform as blockPerform,
+} from "../../../lib/searches/block/details";
+import { PureFunctionTester } from "../../../types";
 
-describe("block", () => {
+describe("details", () => {
   let appTester: ReturnType<typeof createAppTester>;
-  let perform: typeof blockPerform;
+  let perform: PureFunctionTester<BlockDetailsInput, BlockDetailsResponse>;
 
   beforeEach(() => {
     appTester = createAppTester(App);
     tools.env.inject();
-    perform = App.searches.blockDetails.operation
-      .perform as typeof blockPerform;
+    perform = (input) =>
+      appTester(
+        App.searches.blockDetails.operation.perform as typeof blockPerform,
+        input
+      );
   });
 
   it("should return block details for finality input", async () => {
-    const [result] = await appTester(perform, {
+    const [result] = await perform({
       inputData: { finality: Finality.FINAL },
     });
 
@@ -24,6 +32,7 @@ describe("block", () => {
   });
 
   it("should return block details for block ID input", async () => {
+    const [result] = await perform({
     const [result] = await appTester(perform, {
       inputData: { blockId: 80946664 },
     });
@@ -32,7 +41,7 @@ describe("block", () => {
   });
 
   it("should return block details for empty input", async () => {
-    const [result] = await appTester(perform, {
+    const [result] = await perform({
       inputData: {},
     });
 
